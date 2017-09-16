59d5ae22914ad2ac907b3a72f46542d70d55abd1
Tommy Speigner <tspeigner@gmail.com>
2017-09-15 22:50:53 -0700

    adding template file to config class and fixing hiera data

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,4 +7,11 @@
 # @example
 #   include win_tomcat::config
 class win_tomcat::config {
+  include win_tomcat
+  file{ $win_tomcat::conffile:
+    ensure  => file,
+    content => epp('win_tomcat/catalina.properties.epp'),
+    #basic template file that comes with default install.
+    #Customization is coming soon
+  }
 }
