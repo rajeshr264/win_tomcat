8c1c176330c9034a6db2e40ea5123a495f4d8053
Tommy Speigner <tspeigner@gmail.com>
2017-09-19 09:18:52 -0700

    changing case statement in install

--- manifests/install.pp
+++ manifests/install.pp
@@ -9,23 +9,23 @@
 class win_tomcat::install {
   # Include main class to use its parameters
   include win_tomcat
-  if $win_tomcat::version != undef {
-    case $win_tomcat::version {
-      '/7.\d.\d\d': {
+  if $win_tomcat::major_version and $win_tomcat::minor_version {
+    case $win_tomcat::major_version {
+      '7': {
         package { 'tomcat':
           ensure          => $win_tomcat::ensure,
           provider        => chocolatey,
           install_options => ['-params', '--version', "${win_tomcat::version}", '"', "unzipLocation=${$win_tomcat::install_path}", '"'],
         }
       }
-      '/8.\d.\d\d': {
+      '8': {
         package { 'tomcat':
           ensure          => $win_tomcat::ensure,
           provider        => chocolatey,
           install_options => ['-params', '"', "unzipLocation=${$win_tomcat::install_path}", '"'],
         }
       }
-      '9.0.0-rc': {
+      '9': {
         package { 'tomcat':
           ensure          => $win_tomcat::ensure,
           provider        => chocolatey,
