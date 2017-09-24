2335c6ffb95f00623c7325832e6a7b5323693f4c
Tommy Speigner <tspeigner@gmail.com>
2017-09-24 15:10:11 -0700

    basic training

--- manifests/install.pp
+++ manifests/install.pp
@@ -10,17 +10,22 @@
 class win_tomcat::install {
   # Include main class to use its parameters
   include win_tomcat
-  if $win_tomcat::version_major == '7' or '9' {
-    package { 'tomcat':
-      ensure          => installed,
-      provider        => chocolatey,
-      install_options => ['-params', '"', "--version ${win_tomcat::version} unzipLocation=${$win_tomcat::install_path}", '"'],
-    }
-  } else {
-    package { 'tomcat':
+  package { 'tomcat':
       ensure          => installed,
       provider        => chocolatey,
       install_options => ['-params', '"', "unzipLocation=${$win_tomcat::install_path}", '"'],
-    }
   }
+#  if $win_tomcat::version_major == '7' or '9' {
+#    package { 'tomcat':
+#      ensure          => installed,
+#      provider        => chocolatey,
+#      install_options => ['-params', '"', "--version ${win_tomcat::version} unzipLocation=${$win_tomcat::install_path}", '"'],
+#    }
+#  } else {
+#    package { 'tomcat':
+#      ensure          => installed,
+#      provider        => chocolatey,
+#      install_options => ['-params', '"', "unzipLocation=${$win_tomcat::install_path}", '"'],
+#    }
+#  }
 }
