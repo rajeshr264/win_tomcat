ee8011fd6e8d3f2566894eb9fccc0053ef11b36c
Tommy Speigner <tspeigner@gmail.com>
2017-09-24 12:29:49 -0700

    changing from a case statement to if statement.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,44 +1,26 @@
 # win_tomcat::install
 #
-# A description of what this class does
+# This class installs Tomcat.  To specify a version change the values
+# in the {modulepath}/data/common.yaml.  If version 7 or 9 it tells chocolatey
+# to install with the whole version number otherwise it defaults to 'latest' version 8.
 #
-# @summary A short summary of the purpose of this class
 #
 # @example
 #   include win_tomcat::install
 class win_tomcat::install {
   # Include main class to use its parameters
   include win_tomcat
-  if $win_tomcat::major_version and $win_tomcat::minor_version {
-    case $win_tomcat::major_version {
-      '7': {
-        package { 'tomcat':
-          ensure          => $win_tomcat::ensure,
-          provider        => chocolatey,
-          install_options => ['-params', '--version', "${win_tomcat::version}", '"', "unzipLocation=${$win_tomcat::install_path}", '"'],
-        }
-      }
-      '8': {
-        package { 'tomcat':
-          ensure          => $win_tomcat::ensure,
-          provider        => chocolatey,
-          install_options => ['-params', '"', "unzipLocation=${$win_tomcat::install_path}", '"'],
-        }
-      }
-      '9': {
-        package { 'tomcat':
-          ensure          => $win_tomcat::ensure,
-          provider        => chocolatey,
-          install_options => ['-params', '--version', "${win_tomcat::version}", '"', "unzipLocation=${$win_tomcat::install_path}", '"'],
-      }
+  if $win_tomcat::version_major == '7' or '9' {
+    package { 'tomcat':
+      ensure          => installed,
+      provider        => chocolatey,
+      install_options => ['-params', '--version', "${win_tomcat::version}", '"', "unzipLocation=${$win_tomcat::install_path}", '"'],
     }
-      default: {
-        package { 'tomcat':
-          ensure          => installed,
-          provider        => chocolatey,
-          install_options => ['-params', '"', "unzipLocation=${$win_tomcat::install_path}", '"'],
-        }
-      }
+  } else {
+    package { 'tomcat':
+      ensure          => installed,
+      provider        => chocolatey,
+      install_options => ['-params', "${win_tomcat::version}", '"', "unzipLocation=${$win_tomcat::install_path}", '"'],
     }
   }
 }
