a76dad785095ef392b56e9c7af529466320866af
Tommy Speigner <tspeigner@gmail.com>
2017-09-25 16:17:57 -0700

    fixing fact to search --local-only in choco

--- lib/facter/tomcat_version.rb
+++ lib/facter/tomcat_version.rb
@@ -3,6 +3,6 @@ Facter.add(:tomcat_version) do
     # Using choco list on the package namage, then splitting the results (on space)
     # then getting the 4 item.  This might change later if choco list has a different output
     # but this works for now.
-    Facter::Util::Resolution.exec('choco list tomcat').split[3]
+    Facter::Util::Resolution.exec('choco list --local-only tomcat').split[3]
   end
 end
