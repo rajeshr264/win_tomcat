730ccfe78473bc09ffa32c438437efa828b7c0e3
Tommy Speigner <tspeigner@gmail.com>
2017-09-26 10:12:32 -0700

    trying new fact

--- lib/facter/tomcat_version.1.rb
+++ lib/facter/tomcat_version.1.rb
@@ -3,6 +3,6 @@ Facter.add(:tomcat_version1) do
     # Using choco list on the package namage, then splitting the results (on space)
     # then getting the 4 item.  This might change later if choco list has a different output
     # but this works for now.
-    Facter::Util::Resolution.exec("puppet resource package apache-httpd --to_yaml")['package']['tomcat']['ensure']
+    Facter::Util::Resolution.exec("puppet resource package apache-httpd --to_yaml"['package']['tomcat']['ensure'])
   end
 end
