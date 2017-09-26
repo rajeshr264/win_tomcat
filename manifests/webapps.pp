11daf1bbb63c449737c0ef6b0aab0cf1c903deed
Tommy Speigner <tspeigner@gmail.com>
2017-09-26 15:44:31 -0700

    many changes.  webapps and templates

---
+++ manifests/webapps.pp
@@ -0,0 +1,10 @@
+# win_tomcat::webapps
+#
+# Deploy Tomcat applications from this class
+#
+# @summary Deploy your WAR files here.
+#
+# @example
+#   include win_tomcat::webapps
+class win_tomcat::webapps {
+}
