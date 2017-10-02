b9f5a1abba742cac5187a04ab60b62f790adb83c
Tommy Speigner <tspeigner@gmail.com>
2017-10-02 13:22:02 -0700

    adding a second application

--- manifests/webapps.pp
+++ manifests/webapps.pp
@@ -7,10 +7,15 @@
 # @example
 #   include win_tomcat::webapps
 class win_tomcat::webapps {
-  # Sample Web Application being hosted on an external URL
+  # Sample Web Application, using Hiera data for param and value
   # Double slashes for Windows path
   file { "${win_tomcat::catalina_home}\\webapps\\sample.war":
     ensure => file,
     source => $win_tomcat::sample_app_src,
+  }
+  # Sample Web Application, using a direct URL
+  file { "${win_tomcat::catalina_home}\\webapps\\howl-logger":
+    ensure => file,
+    source => 'https://repo1.maven.org/maven2/howl/howl-logger/0.1.11/howl-logger-0.1.11.jar',
   }
 }
