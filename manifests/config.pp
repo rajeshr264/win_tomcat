70c4cbab4cb52918ab4108cb7024c6df0e35e2dd
Tommy Speigner <tspeigner@gmail.com>
2017-10-02 11:06:52 -0700

    adding epp extension to files in config class

--- manifests/config.pp
+++ manifests/config.pp
@@ -17,36 +17,36 @@ class win_tomcat::config {
   # Manage the tomcat-users.xml file.  Based on Puppet template.
   file { $win_tomcat::tcusersxml:
     ensure  => file,
-    content => epp('win_tomcat/tomcat-users.xml'),
+    content => epp('win_tomcat/tomcat-users.xml.epp'),
   }
   # Manage the catalina.properties file.  Based on Puppet template.
   # Security policy permissions for Tomcat
   file { $win_tomcat::catalinapolicy:
     ensure  => file,
-    content => epp('win_tomcat/catalina.policy'),
+    content => epp('win_tomcat/catalina.policy.epp'),
   }
   # Manage the context.xml file.  Based on Puppet template.
   #  
   file { $win_tomcat::contextxml:
     ensure  => file,
-    content => epp ('win_tomcat/context.xml'),
+    content => epp ('win_tomcat/context.xml.epp'),
   }
   # Manage the logging.properties file.  Based on Puppet template.
   # 
   file { $win_tomcat::loggingproperties:
     ensure  => file,
-    content => epp('win_tomcat/logging.properties'),
+    content => epp('win_tomcat/logging.properties.epp'),
   }
   # Manage the server.xml file.  Based on Puppet template. 
   #
   file { $win_tomcat::serverxml:
     ensure  => file,
-    content => epp('win_tomcat/logging.properties'),
+    content => epp('win_tomcat/server.xml.epp'),
   }
   # Manage the server.xml file.  Based on Puppet template. 
   # 
   file { $win_tomcat::webxml:
     ensure  => file,
-    content => epp('win_tomcat/web.xml'),
+    content => epp('win_tomcat/web.xml.epp'),
   }
 }
