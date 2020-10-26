eff16993bf01af63379c73c99adfeb32be5f8ba9
Rajesh Radhakrishnan <enthoughts@gmail.com>
2020-10-25 21:57:26 -0700

    Update webapps.pp

--- manifests/webapps.pp
+++ manifests/webapps.pp
@@ -9,10 +9,15 @@
 class win_tomcat::webapps {
   # Sample Web Application, using Hiera data for param and value
   # Double slashes for Windows path
-  file { "${win_tomcat::catalina_home}\\webapps\\sample.war":
-    ensure => file,
-    source => $win_tomcat::sample_app_src,
+  download_file { "Download tomcat sample app" :
+    url                   => 'https://tomcat.apache.org/tomcat-9.0-doc/appdev/sample/sample.war',
+    destination_directory => '${win_tomcat::catalina_home}\\webapps\\'
   }
+  #class {
+  #file { "${win_tomcat::catalina_home}\\webapps\\sample.war":
+  #  ensure => file,
+  #  source => $win_tomcat::sample_app_src,
+  #}
   # Sample Web Application, using a direct URL
   #file { "${win_tomcat::catalina_home}\\webapps\\sample_apcera.war":
   #  ensure => file,
