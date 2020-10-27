fd29278fd5a238b0d8e31551fdc5a8f7ce7373e8
Rajesh Radhakrishnan <enthoughts@gmail.com>
2020-10-26 20:39:11 -0700

    Update service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -7,9 +7,12 @@
 # @example
 #   include win_tomcat::service
 class win_tomcat::service {
-    
+  include win_tomcat  
   exec { 'tomcat_start_bat':
      command   => "${win_tomcat::catalina_home}\\bin\\startup.bat",
      logoutput => on_failure,
+  }
+   service { '${win_tomcat::service_name':
+     ensure   => running,
   }
 }
