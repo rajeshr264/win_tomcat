4622e4602b6b855574a957eb2868c00b9c8b0808
Rajesh Radhakrishnan <enthoughts@gmail.com>
2020-10-27 08:35:10 -0700

    cleanup

--- manifests/install.pp
+++ manifests/install.pp
@@ -22,11 +22,4 @@ class win_tomcat::install {
   windows_env { "CATALINA_BASE=${win_tomcat::catalina_base}": }
   windows_env { "CATALINA_HOME=${win_tomcat::catalina_home}": }
   
-  # install the service if it is not running
-  #exec { 'tomcat_service_bat':
-  #   command   => "${win_tomcat::catalina_base}\\bin\\service.bat install",
-  #   onlyif    => "get-service -Name ${win_tomcat::version}|%{if ($_.Status -ne 'Running') {exit 0}}",
-  #   provider  => powershell,
-  #}
-  
 }
