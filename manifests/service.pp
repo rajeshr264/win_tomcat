beeafc7a4b2ba7bba2c50d72be312223bcdf8455
Rajesh Radhakrishnan <enthoughts@gmail.com>
2020-10-27 08:34:21 -0700

    kick off tomcat service
    choco installs tomcat & registers the service with name "Tomcast9"

--- manifests/service.pp
+++ manifests/service.pp
@@ -8,11 +8,10 @@
 #   include win_tomcat::service
 class win_tomcat::service {
   include win_tomcat  
-  exec { 'tomcat_start_bat':
-     command   => "${win_tomcat::catalina_home}\\bin\\service.bat install",
-     logoutput => on_failure,
-  }
-   -> service { "${win_tomcat::service_name}":
+  
+  # "choco install tomcat", also registers a service with the name "Tomcat9"
+  service { "${win_tomcat::service_name}":
      ensure   => running,
+     enable => true,
   }
 }
