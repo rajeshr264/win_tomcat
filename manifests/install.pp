0b7a85eca581a78520d45e8db3507cb739db80da
Rajesh Radhakrishnan <enthoughts@gmail.com>
2020-10-27 11:21:25 -0700

    Update install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -13,13 +13,13 @@ class win_tomcat::install {
     ensure          => $win_tomcat::ensure,
     provider        => chocolatey,
   } ->
-  package { 'tomcat':
+  package { 'tomcat_uninstall':
     ensure          => absent,
     provider        => chocolatey,
     install_options => ['--version', "${win_tomcat::version}", '--ignorepackagecodes,', '-y', '-params', '"', "unzipLocation=${win_tomcat::catalina_base}", '"'],
   }
   ->
-  package { 'tomcat':
+  package { 'tomcat_install':
     ensure          => $win_tomcat::ensure,
     provider        => chocolatey,
     install_options => ['--version', "${win_tomcat::version}", '--ignorepackagecodes,' ,'-y', '-params', '"', "unzipLocation=${win_tomcat::catalina_base}", '"'],
